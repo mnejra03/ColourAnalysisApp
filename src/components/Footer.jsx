a21560c634964419d2431b301064d2fa0a52d4da
Nejra Muminović <nejramuminovic0@gmail.com>
2026-03-24 19:46:13 +0100

    Footer.edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
+import './Footer.css'
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container footer-content">
-        <p>
-          © {new Date().getFullYear()} <strong>Nejra Muminović</strong> — Built with ❤️
-          <a href="https://github.com/mnejra03" target="_blank"> GitHub</a>
-        </p>
+      <div className="footer-container">
+
+        <div className="footer-left">
+          <h3>Colour Analysis</h3>
+          <p>
+            Discover your perfect colours and enhance your natural beauty.
+          </p>
+        </div>
+
+        <div className="footer-right">
+          <p className="copyright">
+            © {new Date().getFullYear()} Nejra Muminović ·
+            <a href="https://github.com/mnejra03" target="_blank"> GitHub</a>
+          </p>
+          <p className="small-text">All rights reserved</p>
+        </div>
+
       </div>
     </footer>
   )
